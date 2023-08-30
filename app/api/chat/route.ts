8c7ade57f9510466627b4dbb90be2c9cb6772445
18577447378 <dadg39@qq.com>
2023-08-30 18:35:18 +0800

    first commit

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -5,10 +5,10 @@ import { Configuration, OpenAIApi } from 'openai-edge'
 import { auth } from '@/auth'
 import { nanoid } from '@/lib/utils'
 
-export const runtime = 'edge'
+export const runtime = 'nodejs'
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY
+  apiKey: process.env.OPENAI_API_KEY,
 })
 
 const openai = new OpenAIApi(configuration)
